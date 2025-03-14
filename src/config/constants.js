53206e2c49068aa57696c12ead81f09a02ddfb92
Pawan <pawan@Pawans-MacBook-Air.local>
2025-03-14 12:28:31 +0530

    fixed edtior key issue

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,4 +3,4 @@ export const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-export const EDITOR_KEY = process.env.EDITOR_KEY;
+export const EDITOR_KEY = process.env.NEXT_PUBLIC_EDITOR_KEY;
